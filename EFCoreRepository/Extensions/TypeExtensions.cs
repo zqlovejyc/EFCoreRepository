12258e90bdda11fa463f7781c568a7543577d6f9
zqlovejyc <943620963@qq.com>
2021-02-07 14:27:52 +0800

    移除FastExpressionCompiler，存在部分情况下Expression编译错误；

--- EFCoreRepository/Extensions/TypeExtensions.cs
+++ EFCoreRepository/Extensions/TypeExtensions.cs
@@ -35,14 +35,8 @@ namespace EFCoreRepository.Extensions
         /// </summary>
         /// <param name="this">类型</param>
         /// <returns>Type</returns>
-        public static Type GetCoreType(this Type @this)
-        {
-            if (@this?.IsNullable() == true)
-            {
-                @this = Nullable.GetUnderlyingType(@this);
-            }
-            return @this;
-        }
+        public static Type GetCoreType(this Type @this) =>
+            @this.IsNullable() ? Nullable.GetUnderlyingType(@this) : @this;
         #endregion
 
         #region IsNullable
@@ -51,10 +45,8 @@ namespace EFCoreRepository.Extensions
         /// </summary>
         /// <param name="this">类型</param>
         /// <returns>bool</returns>
-        public static bool IsNullable(this Type @this)
-        {
-            return @this.IsValueType && @this.IsGenericType && @this.GetGenericTypeDefinition() == typeof(Nullable<>);
-        }
+        public static bool IsNullable(this Type @this) =>
+            @this != null && @this.IsValueType && @this.IsGenericType && @this.GetGenericTypeDefinition() == typeof(Nullable<>);
         #endregion
     }
 }
